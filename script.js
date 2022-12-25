b64427c21d7ed2e8a3d37208484091bb5b94e49e
ricantony6 <ricantony66@gmail.com>
2022-12-25 11:13:13 -0300

    links

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function togglemode() {
     const html = document.documentElement
-    html.classList.toggle('light')  
+    html.classList.toggle("light")  
     
     
     //  pegar a imgem
     const img = document.querySelector("#profile img")
 
     // substituir a imagem
-    if (html.classList.contains('light')){
+    if (html.classList.contains("light")){
 
  // se tiver light mode, adicionar a imagem light
-        img.setAttribute('src','./assets/avatar-light.png')
+        img.setAttribute("src","./assets/avatar-light.png")
     } else {
-        img.setAttribute('src','./assets/avatar.png')
+        img.setAttribute("src","./assets/avatar.png")
     }
 // se tiver sem light mode, manter a imagem normal  
 }
